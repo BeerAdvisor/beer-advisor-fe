295eacbc93c4f596bcc9fdfe1f0989529dfd0305
sanjikir <w.ooo.w@mail.ru>
2019-01-22 23:53:22 +0100

    Cheking in

--- src/containers/SearchContainer.tsx
+++ src/containers/SearchContainer.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { MenuItem } from '@material-ui/core';
 import { Tabs } from '../components';
-import { MainForm } from '../Forms';
+import { MainForm } from '../forms';
 
 const SearchContainer = () => {
     // TODO: fetch this from server
     const menuItems = [1, 2, 3, 4];
 
     const layoutProps = {
-        searchFieldPlaceholder: 'Find a beer', // TODO: constants
+        searchFieldPlaceholder: 'Find a beer', // TODO: constants, export to separate logic components
         searchFieldLabel: 'Beer name',
         selectLabel: 'Beer type',
         selectMenuItems: menuItems.map(item => (
