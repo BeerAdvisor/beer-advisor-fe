ad83bdea6f0fd9313aed002b086c4d6fe1e87674
Kirill Shamanin <kirill.shamanin@mewssystems.com>
2019-07-16 21:37:47 +0200

    Fixed after sign in navigation

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ const routes = [
     },
     {
         path: '/login',
-        main: () => <LoginPage />,
+        main: (props: any) => <LoginPage {...props} />,
     },
     {
         path: '/form',
